a54e17054173007bc00ec98d32b2da60b695161b
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-06-23 13:34:22 +0800

    updated code 2306220134

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
 }

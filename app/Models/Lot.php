e3e550a276de1f0f50ddbb1c568d113c1c40f183
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-06-24 14:43:26 +0800

    updated code 24062026243

--- app/Models/Lot.php
+++ app/Models/Lot.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lot extends Model
 {
-    //
+    protected $table = 'lot';
+
+    protected $primaryKey = 'lot_id';
+
+    public $incrementing = true;
+
+    protected $keyType = 'int';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'lot_name',
+        'project_id',
+        'contract_no'
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(
+            Project::class,
+            'project_id',
+            'project_id'
+        );
+    }
+
+    public function deliveries()
+    {
+        return $this->hasMany(
+            Delivery::class,
+            'lot_id',
+            'lot_id'
+        );
+    }
 }

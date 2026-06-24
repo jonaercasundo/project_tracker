5b4815813c27a4b9777748eeba091514404f2115
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-06-24 14:14:37 +0800

    updated code 24062026214

--- app/Models/Keystage.php
+++ app/Models/Keystage.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Keystage extends Model
 {
-    //
+    protected $table = 'keystage';
+
+    protected $primaryKey = 'keystage_id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'keystage_num',
+        'description',
+        'lot_id',
+    ];
+
+    /*
+    |---------------------------------------
+    | Relationships (optional but useful)
+    |---------------------------------------
+    */
+
+    public function lot()
+    {
+        return $this->belongsTo(Lot::class, 'lot_id', 'lot_id');
+    }
+
+    public function deliveries()
+    {
+        return $this->hasMany(Delivery::class, 'keystage_id', 'keystage_id');
+    }
 }

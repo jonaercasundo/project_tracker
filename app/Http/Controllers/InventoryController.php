2a519ed04953433b0d08c5b729729b4f385c019c
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-07-09 12:27:16 +0800

    updated code 090720261227am

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -43,7 +43,14 @@ class InventoryController extends Controller
 
     public function create()
     {
-        dd('create reached');
+        $items = Item::orderBy('item_name')->get();
+
+        $warehouses = Warehouse::orderBy('warehouse_name')->get();
+
+        return view('inventory.create', [
+            'items' => $items,
+            'warehouses' => $warehouses
+        ]);
     }
     public function store(Request $request)
     {

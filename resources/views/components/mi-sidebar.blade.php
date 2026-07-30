6e59e4e6707c296c98fa62a857a6b6dbb93b4518
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-07-30 18:38:06 +0800

    latest code designer_module_index 53

--- resources/views/components/mi-sidebar.blade.php
+++ resources/views/components/mi-sidebar.blade.php
@@ -80,7 +80,7 @@
                         <svg class="w-3.5 h-3.5 shrink-0 transition-colors {{ request()->routeIs('mi_app.settings') ? 'text-blue-600' : 'text-slate-400' }}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
-                        <span>SKU Management</span>
+                        <span>Taxonomy Management</span>
                     </a>
                 </li> 
             </ul>

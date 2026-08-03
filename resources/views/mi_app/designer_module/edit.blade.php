46fb4cfd13a6f83c7b0af092898be42f3d2c3955
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-08-03 15:18:38 +0800

    taxonomy table 37

--- resources/views/mi_app/designer_module/edit.blade.php
+++ resources/views/mi_app/designer_module/edit.blade.php
@@ -2,7 +2,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@500;600&display=swap" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.4.3/dist/css/tom-select.css" rel="stylesheet">
     <style>
         .tx-console {
             --tx-bg: #F5F6F3;
@@ -642,7 +642,7 @@
 
         </div>
     </div>
-
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.4.3/dist/js/tom-select.complete.min.js"></script>
     <script>
         function addImageLink() {
             var container = document.getElementById('imageLinks');

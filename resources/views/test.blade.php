32d4158d8f6048569d8c1289f0876fa343b423da
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-08-04 15:32:27 +0800

    stock out 12

---
+++ resources/views/test.blade.php
@@ -0,0 +1,17 @@
+{{-- resources/views/test-drive.blade.php --}}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Drive Test</title>
+</head>
+<body>
+
+<h2>Google Drive Test</h2>
+
+<img
+    src="https://drive.google.com/uc?export=view&id=1t3P4rbW6Sxg7cs-nbvig5hjDfSKQkZ77"
+    width="500">
+
+</body>
+</html>

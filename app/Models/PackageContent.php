5451da02dbb234a772da8690a73ab899ce594045
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-08-13 13:22:51 +0800

    delivery 36

--- app/Models/PackageContent.php
+++ app/Models/PackageContent.php
@@ -7,18 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 class PackageContent extends Model
 {
     protected $table = 'package_content';
+
+    public $timestamps = false;
+
+    public $incrementing = false;
+
+    protected $primaryKey = null;
+
     protected $fillable = [
         'package_id',
         'item_id',
         'qty',
         'qty_teachers_manual',
     ];
+
     public function package()
     {
-        return $this->belongsTo(Package::class, 'package_id', 'package_id');
+        return $this->belongsTo(
+            Package::class,
+            'package_id',
+            'package_id'
+        );
     }
+
     public function item()
     {
-        return $this->belongsTo(Item::class, 'item_id');
+        return $this->belongsTo(
+            Item::class,
+            'item_id',
+            'item_id'
+        );
     }
 }

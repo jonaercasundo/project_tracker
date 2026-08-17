8969f1d5f81a6f436ca716b566e4e302648c861e
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-08-17 13:29:38 +0800

    MI App Generate QR Codes 08

--- app/Http/Controllers/PublicProductController.php
+++ app/Http/Controllers/PublicProductController.php
@@ -18,7 +18,7 @@ class PublicProductController extends Controller
 
         $product->load(['category', 'subCategory', 'collection', 'images']);
 
-        return view('public.product-show', [
+        return view('product-show', [
             'product' => $product,
         ]);
     }

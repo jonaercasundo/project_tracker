6febe25d60b3d2be49ed310dcda116b82eba9124
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-09-14 12:00:14 +0800

    sample

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        URL::forceRootUrl(config('app.url'));
+        URL::forceScheme(parse_url(config('app.url'), PHP_URL_SCHEME));
+    }
+}

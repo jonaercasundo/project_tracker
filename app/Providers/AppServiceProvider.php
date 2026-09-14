eb3a6cb169beb77bb8dab90e0206155ca717e4ba
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-09-14 11:47:42 +0800

    status summary 9142026114701

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (app()->environment(['production', 'local'])) {
             URL::forceRootUrl(config('app.url'));
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 }

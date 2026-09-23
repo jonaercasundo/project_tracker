30195428a8a4fc519c9d3fe09859ccec07e394e8
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-09-23 13:45:07 +0800

    bidding docs revisions 05

--- resources/views/operation/bidding/partials/_forms.blade.php
+++ resources/views/operation/bidding/partials/_forms.blade.php
@@ -522,7 +522,7 @@
                             name="approved_budget_contract_abc"
                             class="bf-input currency"
                             value="{{ old('approved_budget_contract_abc', $project->approved_budget_contract_abc ?? '') }}"
-                            placeholder="0.00" readonly
+                            placeholder="0.00"
                         >
                 </div>
             </div>

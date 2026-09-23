35a4118739bdeec44c71c64eaa8e43f216781aac
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-09-23 13:52:36 +0800

    bidding docs revisions 09

--- resources/views/operation/bidding/partials/_forms.blade.php
+++ resources/views/operation/bidding/partials/_forms.blade.php
@@ -520,7 +520,6 @@
                             type="number"
                             id="approved_budget_contract_abc"
                             name="approved_budget_contract_abc"
-                            class="bf-input"
                         >
                 </div>
             </div>
